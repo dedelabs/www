5e00e20d75909057cb2fec57c736a13641af9f49
andreareginato <andrea.reginato@gmail.com>
2021-02-15 09:31:00 +0100

    reviewing content

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -6324,7 +6324,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58290" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49489" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
